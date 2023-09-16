a0ed47dbf12baf484e3cd0bbad905d1484c03065
S M Rahaman <45679012+Dr-S-K-M-Rahaman@users.noreply.github.com>
2023-09-16 18:19:09 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -13,7 +13,7 @@ contract MyToken is ERC20, Ownable {
     }
     
     // function to allow admin to transfer *any* BEP20 tokens from this contract..
-    function transferAnyBEP20Tokens(address tokenAddress, address recipient, uint256 amount) public onlyOwner {
+    function transferAnyERC20Tokens(address tokenAddress, address recipient, uint256 amount) public onlyOwner {
         require(amount > 0, "$EARN: amount must be greater than 0");
         require(recipient != address(0), "$EARN: recipient is the zero address");
         require(tokenAddress != address(this), "$EARN: Not possible to transfer $EARN");
